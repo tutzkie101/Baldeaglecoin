c068d4bff5827483deb1a6240b9cc70f9e4aebbe
brent dugan <brentcdugan@gmail.com>
2021-09-09 19:53:20 -0700

    major rename

--- INSTALL.md
+++ INSTALL.md
@@ -1,5 +1,5 @@
-Building Litecoin
+Building Baldeaglecoin
 ================
 
 See doc/build-*.md for instructions on building the various
-elements of the Litecoin Core reference implementation of Litecoin.
+elements of the Baldeaglecoin Core reference implementation of Baldeaglecoin.
--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,11 +7,11 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$BINDIR/litecoind}
-BITCOINCLI=${BITCOINCLI:-$BINDIR/litecoin-cli}
-BITCOINTX=${BITCOINTX:-$BINDIR/litecoin-tx}
-WALLET_TOOL=${WALLET_TOOL:-$BINDIR/litecoin-wallet}
-BITCOINQT=${BITCOINQT:-$BINDIR/qt/litecoin-qt}
+BITCOIND=${BITCOIND:-$BINDIR/baldeaglecoind}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/baldeaglecoin-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/baldeaglecoin-tx}
+WALLET_TOOL=${WALLET_TOOL:-$BINDIR/baldeaglecoin-wallet}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/baldeaglecoin-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 
